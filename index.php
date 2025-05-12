4ff427b7ec8d079b05bed27a9e7311ba72034ba8
devnight <devnight.learn@gmail.com>
2025-05-13 03:29:55 +0330

    fix bug in section discount product

--- index.php
+++ index.php
@@ -263,14 +263,222 @@
                                     <h3 class="text-sm font-medium leading-[24px] ">گوشی موبایل سامسونگ Galaxy S25 Ultra 5G ظرفیت 256 گیگابایت رم 12 گیگابایت - ویتنام</h3>
                                 </a>
                                 <div class="flex items-center justify-between ">
-                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded">2%</span>
+                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded font-yekanbakh-regular">2%</span>
                                     <div class="flex items-end gap-2 flex-col ">
                                         <div class="flex items-center gap-2 ">
-                                            <span class="text-sm font-bold">25,900,000</span>
+                                            <span class="text-sm font-bold font-yekanbakh-regular">25,900,000</span>
                                             <span class="text-xs">تومان</span>
                                         </div>
                                         <div class="flex items-center gap-2  ">
-                                            <span class="text-[11px] text-gray-400 font-bold line-through ">25,390,000</span>
+                                            <span class="text-[11px] text-gray-400 font-bold line-through font-yekanbakh-regular">25,390,000</span>
+                                            <span class="text-[11px] text-gray-400">تومان</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="border-l border-gray-200 p-2">
+                            <div class="relative w-full">
+                                <a href=""> <img class="w-full" src="<?php echo get_template_directory_uri() . '/assets/img/pro01.webp' ?>" alt=""></a>
+                            </div>
+                            <div class="mt-4 flex flex-col gap-2">
+                                <a href="" class="yekanbakh-en line-clamp-2  w-full text-xs font-medium  -tracking-0.5 text-primary-shade-1 2md:h-[50px] 2md:text-sm 2md:leading-[27px] 2md:text-primary-shade-1">
+                                    <h3 class="text-sm font-medium leading-[24px] ">گوشی موبایل اپل iPhone 15 Pro Max دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت</h3>
+                                </a>
+                                <div class="flex items-center justify-between ">
+                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded font-yekanbakh-regular">5%</span>
+                                    <div class="flex items-end gap-2 flex-col ">
+                                        <div class="flex items-center gap-2 ">
+                                            <span class="text-sm font-bold font-yekanbakh-regular">89,900,000</span>
+                                            <span class="text-xs">تومان</span>
+                                        </div>
+                                        <div class="flex items-center gap-2  ">
+                                            <span class="text-[11px] text-gray-400 font-bold line-through font-yekanbakh-regular">94,500,000</span>
+                                            <span class="text-[11px] text-gray-400">تومان</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="border-l border-gray-200 p-2">
+                            <div class="relative w-full">
+                                <a href=""> <img class="w-full" src="<?php echo get_template_directory_uri() . '/assets/img/pro01.webp' ?>" alt=""></a>
+                            </div>
+                            <div class="mt-4 flex flex-col gap-2">
+                                <a href="" class="yekanbakh-en line-clamp-2  w-full text-xs font-medium  -tracking-0.5 text-primary-shade-1 2md:h-[50px] 2md:text-sm 2md:leading-[27px] 2md:text-primary-shade-1">
+                                    <h3 class="text-sm font-medium leading-[24px] ">گوشی موبایل شیائومی مدل Redmi Note 13 Pro دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت</h3>
+                                </a>
+                                <div class="flex items-center justify-between ">
+                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded font-yekanbakh-regular">8%</span>
+                                    <div class="flex items-end gap-2 flex-col ">
+                                        <div class="flex items-center gap-2 ">
+                                            <span class="text-sm font-bold font-yekanbakh-regular">15,490,000</span>
+                                            <span class="text-xs">تومان</span>
+                                        </div>
+                                        <div class="flex items-center gap-2  ">
+                                            <span class="text-[11px] text-gray-400 font-bold line-through font-yekanbakh-regular">16,900,000</span>
+                                            <span class="text-[11px] text-gray-400">تومان</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="border-l border-gray-200 p-2">
+                            <div class="relative w-full">
+                                <a href=""> <img class="w-full" src="<?php echo get_template_directory_uri() . '/assets/img/pro01.webp' ?>" alt=""></a>
+                            </div>
+                            <div class="mt-4 flex flex-col gap-2">
+                                <a href="" class="yekanbakh-en line-clamp-2  w-full text-xs font-medium  -tracking-0.5 text-primary-shade-1 2md:h-[50px] 2md:text-sm 2md:leading-[27px] 2md:text-primary-shade-1">
+                                    <h3 class="text-sm font-medium leading-[24px] ">گوشی موبایل هوآوی مدل Nova 11 Pro دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت</h3>
+                                </a>
+                                <div class="flex items-center justify-between ">
+                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded font-yekanbakh-regular">12%</span>
+                                    <div class="flex items-end gap-2 flex-col ">
+                                        <div class="flex items-center gap-2 ">
+                                            <span class="text-sm font-bold font-yekanbakh-regular">19,800,000</span>
+                                            <span class="text-xs">تومان</span>
+                                        </div>
+                                        <div class="flex items-center gap-2  ">
+                                            <span class="text-[11px] text-gray-400 font-bold line-through font-yekanbakh-regular">22,500,000</span>
+                                            <span class="text-[11px] text-gray-400">تومان</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="border-l border-gray-200 p-2">
+                            <div class="relative w-full">
+                                <a href=""> <img class="w-full" src="<?php echo get_template_directory_uri() . '/assets/img/pro01.webp' ?>" alt=""></a>
+                            </div>
+                            <div class="mt-4 flex flex-col gap-2">
+                                <a href="" class="yekanbakh-en line-clamp-2  w-full text-xs font-medium  -tracking-0.5 text-primary-shade-1 2md:h-[50px] 2md:text-sm 2md:leading-[27px] 2md:text-primary-shade-1">
+                                    <h3 class="text-sm font-medium leading-[24px] ">گوشی موبایل نوکیا مدل G42 5G دو سیم کارت ظرفیت 128 گیگابایت و رم 6 گیگابایت</h3>
+                                </a>
+                                <div class="flex items-center justify-between ">
+                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded font-yekanbakh-regular">15%</span>
+                                    <div class="flex items-end gap-2 flex-col ">
+                                        <div class="flex items-center gap-2 ">
+                                            <span class="text-sm font-bold font-yekanbakh-regular">7,290,000</span>
+                                            <span class="text-xs">تومان</span>
+                                        </div>
+                                        <div class="flex items-center gap-2  ">
+                                            <span class="text-[11px] text-gray-400 font-bold line-through font-yekanbakh-regular">8,590,000</span>
+                                            <span class="text-[11px] text-gray-400">تومان</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="border-l border-gray-200 p-2">
+                            <div class="relative w-full">
+                                <a href=""> <img class="w-full" src="<?php echo get_template_directory_uri() . '/assets/img/pro01.webp' ?>" alt=""></a>
+                            </div>
+                            <div class="mt-4 flex flex-col gap-2">
+                                <a href="" class="yekanbakh-en line-clamp-2  w-full text-xs font-medium  -tracking-0.5 text-primary-shade-1 2md:h-[50px] 2md:text-sm 2md:leading-[27px] 2md:text-primary-shade-1">
+                                    <h3 class="text-sm font-medium leading-[24px] ">گوشی موبایل وان پلاس مدل Nord CE3 Lite 5G دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت</h3>
+                                </a>
+                                <div class="flex items-center justify-between ">
+                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded font-yekanbakh-regular">10%</span>
+                                    <div class="flex items-end gap-2 flex-col ">
+                                        <div class="flex items-center gap-2 ">
+                                            <span class="text-sm font-bold font-yekanbakh-regular">11,890,000</span>
+                                            <span class="text-xs">تومان</span>
+                                        </div>
+                                        <div class="flex items-center gap-2  ">
+                                            <span class="text-[11px] text-gray-400 font-bold line-through font-yekanbakh-regular">13,200,000</span>
+                                            <span class="text-[11px] text-gray-400">تومان</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="border-l border-gray-200 p-2">
+                            <div class="relative w-full">
+                                <a href=""> <img class="w-full" src="<?php echo get_template_directory_uri() . '/assets/img/pro01.webp' ?>" alt=""></a>
+                            </div>
+                            <div class="mt-4 flex flex-col gap-2">
+                                <a href="" class="yekanbakh-en line-clamp-2  w-full text-xs font-medium  -tracking-0.5 text-primary-shade-1 2md:h-[50px] 2md:text-sm 2md:leading-[27px] 2md:text-primary-shade-1">
+                                    <h3 class="text-sm font-medium leading-[24px] ">گوشی موبایل موتورولا مدل Edge 40 Neo دو سیم کارت ظرفیت 256 گیگابایت و رم 12 گیگابایت</h3>
+                                </a>
+                                <div class="flex items-center justify-between ">
+                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded font-yekanbakh-regular">18%</span>
+                                    <div class="flex items-end gap-2 flex-col ">
+                                        <div class="flex items-center gap-2 ">
+                                            <span class="text-sm font-bold font-yekanbakh-regular">16,490,000</span>
+                                            <span class="text-xs">تومان</span>
+                                        </div>
+                                        <div class="flex items-center gap-2  ">
+                                            <span class="text-[11px] text-gray-400 font-bold line-through font-yekanbakh-regular">20,100,000</span>
+                                            <span class="text-[11px] text-gray-400">تومان</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="border-l border-gray-200 p-2">
+                            <div class="relative w-full">
+                                <a href=""> <img class="w-full" src="<?php echo get_template_directory_uri() . '/assets/img/pro01.webp' ?>" alt=""></a>
+                            </div>
+                            <div class="mt-4 flex flex-col gap-2">
+                                <a href="" class="yekanbakh-en line-clamp-2  w-full text-xs font-medium  -tracking-0.5 text-primary-shade-1 2md:h-[50px] 2md:text-sm 2md:leading-[27px] 2md:text-primary-shade-1">
+                                    <h3 class="text-sm font-medium leading-[24px] ">گوشی موبایل ریلمی مدل C53 دو سیم کارت ظرفیت 128 گیگابایت و رم 6 گیگابایت</h3>
+                                </a>
+                                <div class="flex items-center justify-between ">
+                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded font-yekanbakh-regular">20%</span>
+                                    <div class="flex items-end gap-2 flex-col ">
+                                        <div class="flex items-center gap-2 ">
+                                            <span class="text-sm font-bold font-yekanbakh-regular">5,890,000</span>
+                                            <span class="text-xs">تومان</span>
+                                        </div>
+                                        <div class="flex items-center gap-2  ">
+                                            <span class="text-[11px] text-gray-400 font-bold line-through font-yekanbakh-regular">7,390,000</span>
+                                            <span class="text-[11px] text-gray-400">تومان</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="border-l border-gray-200 p-2">
+                            <div class="relative w-full">
+                                <a href=""> <img class="w-full" src="<?php echo get_template_directory_uri() . '/assets/img/pro01.webp' ?>" alt=""></a>
+                            </div>
+                            <div class="mt-4 flex flex-col gap-2">
+                                <a href="" class="yekanbakh-en line-clamp-2  w-full text-xs font-medium  -tracking-0.5 text-primary-shade-1 2md:h-[50px] 2md:text-sm 2md:leading-[27px] 2md:text-primary-shade-1">
+                                    <h3 class="text-sm font-medium leading-[24px] ">گوشی موبایل گوگل مدل Pixel 8 Pro دو سیم کارت ظرفیت 256 گیگابایت و رم 12 گیگابایت</h3>
+                                </a>
+                                <div class="flex items-center justify-between ">
+                                    <span class="bg-[#da1c28] text-white text-sm font-medium px-2 py-1 rounded font-yekanbakh-regular">25%</span>
+                                    <div class="flex items-end gap-2 flex-col ">
+                                        <div class="flex items-center gap-2 ">
+                                            <span class="text-sm font-bold font-yekanbakh-regular">45,900,000</span>
+                                            <span class="text-xs">تومان</span>
+                                        </div>
+                                        <div class="flex items-center gap-2  ">
+                                            <span class="text-[11px] text-gray-400 font-bold line-through font-yekanbakh-regular">61,200,000</span>
                                             <span class="text-[11px] text-gray-400">تومان</span>
                                         </div>
                                     </div>
